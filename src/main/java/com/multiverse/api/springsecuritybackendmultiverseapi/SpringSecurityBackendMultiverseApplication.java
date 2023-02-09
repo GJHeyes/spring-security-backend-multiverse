3aacf4b4747bea6d7fad680376d90c22fb2867ae
GJHeyes <gregory-heyes@hotmail.com>
2023-02-09 11:08:34 +0000

    first commit

---
+++ src/main/java/com/multiverse/api/springsecuritybackendmultiverseapi/SpringSecurityBackendMultiverseApplication.java
@@ -0,0 +1,13 @@
+package com.multiverse.api.springsecuritybackendmultiverseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBackendMultiverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBackendMultiverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multiverse/api/springsecuritybackendmultiverseapi/SpringSecurityBackendMultiverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiverse.api.springsecuritybackendmultiverseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBackendMultiverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
