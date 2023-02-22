81030af89ccc3d5cbbe85fb7cc5a13618239d52e
Ambridge88 <daniellelinaker@live.co.uk>
2023-02-22 13:49:15 +0000

    started-entity

---
+++ src/main/java/com/multiverse/api/springsecuritybackendmultiverseapi/Role/Role.java
@@ -0,0 +1,16 @@
+package com.multiverse.api.springsecuritybackendmultiverseapi.Role;
+
+public class Role {
+    private String name;
+    private String email;
+    private String password;
+
+    public Role(String name, String email, String password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+    }
+}
+
+
