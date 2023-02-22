f16f7ecba080a0526e7da04ce3ec6f55e0b5efa3
GJHeyes <gregory-heyes@hotmail.com>
2023-02-22 15:28:20 +0000

    Updated Controller,service, repo, and model

--- src/main/java/com/multiverse/api/springsecuritybackendmultiverseapi/user/UserServiceImpl.java
+++ src/main/java/com/multiverse/api/springsecuritybackendmultiverseapi/user/UserServiceImpl.java
@@ -1,10 +1,10 @@
 package com.multiverse.api.springsecuritybackendmultiverseapi.user;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
