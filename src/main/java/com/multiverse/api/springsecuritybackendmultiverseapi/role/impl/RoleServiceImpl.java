a8415363fe9840fa20a43eafc3e8e821e2f8818b
Ambridge88 <daniellelinaker@live.co.uk>
2023-02-24 16:08:31 +0000

    updated roles

--- src/main/java/com/multiverse/api/springsecuritybackendmultiverseapi/role/impl/RoleServiceImpl.java
+++ src/main/java/com/multiverse/api/springsecuritybackendmultiverseapi/role/impl/RoleServiceImpl.java
@@ -1,9 +1,11 @@
 package com.multiverse.api.springsecuritybackendmultiverseapi.role.impl;
 
+import com.multiverse.api.springsecuritybackendmultiverseapi.recipes.Recipes;
 import com.multiverse.api.springsecuritybackendmultiverseapi.role.RoleRepository;
 import com.multiverse.api.springsecuritybackendmultiverseapi.role.RoleService;
 import com.multiverse.api.springsecuritybackendmultiverseapi.role.Roles;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.BeanDefinitionDsl;
 
 import java.util.List;
 
@@ -11,22 +13,45 @@ public class RoleServiceImpl implements RoleService {
 
     @Autowired
     private RoleRepository roleRepository;
+
     @Override
     public List<Roles> getAllRoles() {
         return (List<Roles>) roleRepository.findAll();
     }
+
     @Override
     public Roles getRoleById(int roleId) {
         return roleRepository.findById(roleId).orElse(null);
 
     }
+
     @Override
-    public Roles addOrUpdateRole(Roles role) {
-        return null;
+    public Roles addRole(Roles role) {
+
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Roles updateRole(Roles role) {
+        return roleRepository.save(role);
     }
 
     @Override
     public Roles deleteRole(int roleId) throws Exception {
-        return null;
+        Roles deletedRole = (null);
+        try {
+            deletedRole = roleRepository.findById(roleId).orElse(null);
+            if (deletedRole == null) {
+                throw new Exception("role dose not exist");
+
+            } else {
+                roleRepository.deleteById(roleId);
+            }
+        } catch (Exception ex) {
+            throw ex;
+
+        } return deletedRole;
     }
 }
+
+
