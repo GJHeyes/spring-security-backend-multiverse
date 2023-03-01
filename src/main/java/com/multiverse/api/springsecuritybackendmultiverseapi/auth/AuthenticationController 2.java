2804fd19a55d09ef8c22d898c488b19485271f09
GJHeyes <gregory-heyes@hotmail.com>
2023-03-01 10:13:08 +0000

    Deleted incorrect files

---
+++ src/main/java/com/multiverse/api/springsecuritybackendmultiverseapi/auth/AuthenticationController 2.java
@@ -0,0 +1,28 @@
+package com.multiverse.api.springsecuritybackendmultiverseapi.auth;
+
+import com.multiverse.api.springsecuritybackendmultiverseapi.user.UserRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody UserRequest request
+    ){
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request
+    ){
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+}
---
+++ src/main/java/com/multiverse/api/springsecuritybackendmultiverseapi/auth/AuthenticationService 2.java
@@ -0,0 +1,59 @@
+package com.multiverse.api.springsecuritybackendmultiverseapi.auth;
+
+import com.multiverse.api.springsecuritybackendmultiverseapi.role.Role;
+import com.multiverse.api.springsecuritybackendmultiverseapi.role.RoleRepository;
+import com.multiverse.api.springsecuritybackendmultiverseapi.user.User;
+import com.multiverse.api.springsecuritybackendmultiverseapi.user.UserRepository;
+import com.multiverse.api.springsecuritybackendmultiverseapi.user.UserRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(UserRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(roleRepository.findById(2).orElseGet(Role::new))
+                .recipes(new ArrayList<>())
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
